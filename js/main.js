f8726e0f7ff9557451ef36c22b6e1598fdf543e2
AlexayStar <66807328+AlexayStar@users.noreply.github.com>
2022-11-11 15:22:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const headerTop = document.querySelector('.header_top');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('menu_active');
+    headerTop.classList.toggle('header_top--active')
+});
+
+var mixer = mixitup('.portfolio_content');
